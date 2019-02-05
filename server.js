f6b6852f89711b2d098ccc90f5d8856ae39cc3ce
Rich Lane <richlanedesign@gmail.com>
2019-02-04 22:40:16 -0700

    schema seeds

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
